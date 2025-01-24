b4bd09b360872aaa39dc4bd5f267044b24ca281a
Filippos Kogiantis <filipposlk15@gmail.com>
2025-01-23 19:52:53 -0500

    wrote user side of sending/recieving messages

--- player.c
+++ player.c
@@ -1,9 +1,8 @@
 #include "player.h"
 #include "client_server.h"
+#include "pipe_networking.h"
 
-int connect_to_server(){
-  char * pp_name = (char *) calloc(sizeof(char),10);
-  sprintf(pp_name, "%d", getpid());
+int connect_to_server(char * pp_name){
   printf("sending pp\n");
   send_pp_name(pp_name);
   printf("sent pp\n");
@@ -12,6 +11,16 @@ int connect_to_server(){
 
 
 int main(){
-  connect_to_server();
+  char * pp_name = (char *) calloc(sizeof(char),10);
+  sprintf(pp_name, "./%d", getpid());
+  connect_to_server(pp_name);
+  char * message = (char *) calloc(sizeof(char),200);
+//  while (1){
+//    recieve_message(pp_name,message);
+//    printf(message);
+//    fgets(message,64,stdin);
+//    sscanf(message,"%[^\n]",message);
+//    send_message(pp_name,message);
+//  }
   return 0;
 }
